da01275523ef498741eda3b6ac8fe802948e07f1
Maxim Pavlov <111360265+2thespace@users.noreply.github.com>
2024-11-20 10:17:56 +0300

    Update decoder.cpp

--- decoder.cpp
+++ decoder.cpp
@@ -12,6 +12,7 @@
 int main(int argc, char** argv) {
     if (argc < 4) {
         std::cout << "try ./decoder path/to/encode/png  encoding_key /path/to/result/png" << std::endl;
+        return 0;
     }
     myImage::Codder merger;
     merger.DecodeAndSave(argv[1], argv[2], argv[3]);
